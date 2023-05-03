77f2d5f55738501602c0d54c4da8779a9bff9736
natyyp <natypiresoliveira@gmail.com>
2023-05-03 16:30:55 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,43 @@
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+
+// const nome = prompt("digite seu nome no campo abaixo por favor!")
+// const email = prompt("digite seu email no campo abaixo porfavor!")
+
+// console.log(prompt(email, `seu e-mail foi cadastrado`))
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}`)
+
+
+// const comidasfav = ["charuto de carne", "maconha com carne", "bolo de cloro e barro", "sorvete de cimento", "frango assalto com barata"]
+// console.log(`comidas favoritas: \n${comidasfav [0]} \n${comidasfav [1]} \n${comidasfav [2]} \n${comidasfav [3]} \n${comidasfav [4]}`)
+// const comidafav2 = prompt("qual a sua comida favorita?")
+// comidasfav[0] = comidafav2
+// console.log(`comidas favoritas: \n${comidasfav [0]} \n${comidasfav [1]} \n${comidasfav [2]} \n${comidasfav [3]} \n${comidasfav [4]}`)
+
+
+// listadetarefa = []
+// const tarefa1 = prompt("digite a 1 tarefa")
+// const tarefa2 = prompt("digite a 2 tarefa")
+// const tarefa3 = prompt("digite a 3 tarefa")
+
